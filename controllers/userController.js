6d090a5dc4295c137614ffa70b9a0afa9286c297
andrzejrumak <ajgltl@gmail.com>
2017-06-15 20:37:21 +0200

    Logout button, Avatar, Add - only if you are logined

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,13 @@ const User = mongoose.model('User');
 const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
-    res.render('login', {title: 'Login'});
+  res.render('login', { title: 'Login' });
 };
 
 exports.registerForm = (req, res) => {
-    res.render('register', {title: 'Register'});
+  res.render('register', { title: 'Register' });
 };
+
 exports.validateRegister = (req, res, next) => {
   req.sanitizeBody('name');
   req.checkBody('name', 'You must supply a name!').notEmpty();
@@ -35,5 +36,5 @@ exports.register = async (req, res, next) => {
   const user = new User({ email: req.body.email, name: req.body.name });
   const register = promisify(User.register, User);
   await register(user, req.body.password);
-  next();
+  next(); // pass to authController.login
 };
