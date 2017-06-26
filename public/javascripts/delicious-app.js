c3133d179f99e0a6f9fcce7f435895e7c9e93eba
andrzejrumak <ajgltl@gmail.com>
2017-06-26 19:49:17 +0200

    Our Hearted Stores displaying

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -5,6 +5,7 @@ import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
 import makeMap from './modules/map';
 import ajaxHeart from './modules/heart';
+
 autocomplete( $('#address'), $('#lat'), $('#lng'));
 
 typeAhead( $('.search'));
--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,10 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 router.get('/map', storeController.mapPages);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+
 //API
+
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.
 mapStores));
