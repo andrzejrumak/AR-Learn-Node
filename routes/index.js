07535fa9002e84199fed80985de8793f298b17fe
andrzejrumak <ajgltl@gmail.com>
2017-06-26 09:16:00 +0200

    User Data to API

--- routes/index.js
+++ routes/index.js
@@ -54,6 +54,8 @@ router.post('/account/reset/:token',
 router.get('/map', storeController.mapPages);
 //API
 router.get('/api/search', catchErrors(storeController.searchStores));
-router.get('/api/stores/near', catchErrors(storeController.mapStores));
-
+router.get('/api/stores/near', catchErrors(storeController.
+mapStores));
+router.post('/api/stores/:id/heart', catchErrors(storeController.
+heartStore));
 module.exports = router;
